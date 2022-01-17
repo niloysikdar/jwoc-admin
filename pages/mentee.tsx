d1a66d00a1be486505c2dee1cb0e166ce2e2725e
Niloy Sikdar <niloysikdar30@gmail.com>
2022-01-18 00:37:22 +0530

    feat: migrated mentee to useEffect

--- pages/mentee.tsx
+++ pages/mentee.tsx
@@ -1,41 +1,55 @@
+import { useState, useEffect } from 'react';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
+import CircularProgress from '@mui/material/CircularProgress';
 
 import { Layout, MenteeCard } from '../components';
 import { MenteeType } from '../types/MenteeType';
 
 import axios from 'axios';
 
-const Mentee = ({ data }: { data: MenteeType[] }) => {
+const Mentee = () => {
+  const [menteeData, setMenteeData] = useState<MenteeType[]>();
+  const [isLoading, setIsloading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.NEXT_PUBLIC_API_URL as string}/mentee`, {
+        headers: {
+          username: process.env.NEXT_PUBLIC_ADMIN_USERNAME as string,
+          password: process.env.NEXT_PUBLIC_ADMIN_PASSWORD as string,
+        },
+      })
+      .then((res) => {
+        setIsloading(false);
+        setMenteeData(res.data.data);
+      })
+      .catch((err) => {
+        setIsloading(false);
+        console.log(err);
+      });
+  }, []);
+
   return (
     <Layout pageName='Mentee'>
-      <Typography variant='h5'>All Mentees ({data?.length || 0})</Typography>
+      <Typography variant='h5'>All Mentees ({menteeData?.length || 0})</Typography>
       <Box className='my-6 lg:w-6/12'>
         <TextField label='Search here' variant='outlined' fullWidth />
       </Box>
-      <Box className='flex flex-wrap gap-6'>
-        {data?.map((menteeData) => (
-          <MenteeCard key={menteeData._id} menteeData={menteeData} />
-        ))}
-      </Box>
+      {isLoading ? (
+        <Box className='flex justify-center mt-10'>
+          <CircularProgress size={50} />
+        </Box>
+      ) : (
+        <Box className='flex flex-wrap gap-6'>
+          {menteeData?.map((data) => (
+            <MenteeCard key={data._id} menteeData={data} />
+          ))}
+        </Box>
+      )}
     </Layout>
   );
 };
 
 export default Mentee;
-
-export const getServerSideProps = async () => {
-  const res = await axios.get(`${process.env.BASE_URL as string}/mentee`, {
-    headers: {
-      username: process.env.ADMIN_USERNAME as string,
-      password: process.env.ADMIN_PASSWORD as string,
-    },
-  });
-
-  return {
-    props: {
-      data: res.data.data,
-    },
-  };
-};
