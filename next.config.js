60f57e9cdc0024b2d52264a3b87a0b26cc456fb6
Niloy Sikdar <niloysikdar30@gmail.com>
2022-01-18 03:42:04 +0530

    fix: recoil messages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
+
+const intercept = require('intercept-stdout');
+
+// safely ignore recoil stdout warning messages
+function interceptStdout(text) {
+  if (text.includes('Duplicate atom key')) {
+    return '';
+  }
+  return text;
+}
+
+// Intercept in dev and prod
+intercept(interceptStdout);
+
 module.exports = {
   reactStrictMode: true,
 };
