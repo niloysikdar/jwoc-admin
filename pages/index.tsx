95fcb42fe053d1f6e131c502d9dcc1726605e415
Niloy Sikdar <niloysikdar30@gmail.com>
2022-01-17 18:41:48 +0530

    fix: migrated the endpoint to .env

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,8 @@ import axios from 'axios';
 import { MentorType } from '../types/MentorType';
 
 const Home = ({ data }: { data: MentorType[] }) => {
+  console.log(data);
+
   return (
     <Layout>
       <Typography variant='h6'>Here is a very good thing</Typography>
@@ -15,7 +17,7 @@ const Home = ({ data }: { data: MentorType[] }) => {
 export default Home;
 
 export const getServerSideProps = async () => {
-  const res = await axios.get('https://jwoc-backend.glitch.me/mentor', {
+  const res = await axios.get(`${process.env.BASE_URL as string}/mentor`, {
     headers: {
       username: process.env.ADMIN_USERNAME as string,
       password: process.env.ADMIN_PASSWORD as string,
